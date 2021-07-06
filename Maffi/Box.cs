8dae7ebbe8ae2ef1b83461ccc2f60672c935f0ba
Giovanni Maffi <giovanni.maffi@studio.unibo.it>
2021-07-07 00:03:47 +0200

    maffi parte c#

---
+++ Maffi/Box.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Maffi
+{
+    public class BoxImpl : Box {
+        private const int MAP_SIZE = 800;
+        private const int BOX_SIZE = 100;
+        //local variables
+        private ImageLoader imgLoader;
+        private double xloc, yloc;
+        private double xdir, ydir;
+        private String name;
+
+        
+        public BoxImpl()
+        {
+            this.xloc = 0;
+            this.yloc = 0;
+            this.xdir = 0;
+            this.ydir = 0;
+        }
+
+        public BoxImpl( String filename)
+        {
+            this.SetImage(filename);
+            this.name = filename;
+        }
+
+        public BoxImpl( String filename,  double xloc,  double yloc)
+        {
+            this.SetImage(filename);
+            this.xloc = xloc * BOX_SIZE;
+            this.yloc = MAP_SIZE - yloc * BOX_SIZE;
+            this.name = filename;
+        }
+
+        public  void SetImage( String filename)
+        {
+            imgLoader = new ImageLoader(filename);
+            this.name = filename;
+        }
+
+        public ImageLoader GetImage()
+        {
+            return imgLoader;
+        }
+     
+        public String GetName
+        {
+            get { return this.name; }
+        }
+       
+        public double XLoc
+        {
+            get { return xloc; }
+            set { xloc = value; }
+        }
+
+        public double YLoc
+        {
+            get { return yloc; }
+            set { yloc = value; }
+        }
+
+        public double XDir
+        {
+            get { return xdir; }
+            set { xdir = value; }
+        }
+
+        public double YDir
+        {
+            get { return ydir; }
+            set { ydir = value; }
+        }
+
+        public int GetWidth
+        {
+            get { return imgLoader.getImgWidth(); }
+        }
+
+        public int GetHeight
+        {
+            get { return imgLoader.getImgHeight(); }
+        }
+
+        public void Paint( Graphics g,  Panel panel)
+        {
+            //if no image has been uploaded, will be drawn an empy square: otherwise will be drawn a square filled with the image 
+            if (imgLoader == null)
+            {
+                g.DrawRectangle(new Pen(Brushes.Black), new Rectangle((int) xloc, (int) yloc, BOX_SIZE, BOX_SIZE));
+            }
+            else
+            {
+                
+                g.DrawImage(imgLoader.getImage(), new Point((int) xloc,(int) yloc));
+            }
+
+        }
+
+        
+        public void Move()
+        {
+            this.xloc += this.xdir;
+            this.yloc += this.ydir;
+        }
+
+    }
+}
---
+++ UnitTest/TestBox.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Maffi;
+
+namespace UnitTest
+{
+    /// <summary>
+    /// Test that verify the correct creation and working of a box.
+    /// </summary>
+    [TestClass]
+    public class TestBox
+    {
+        //local variables for the test
+        private const int XLOC = 5;
+        private const int YLOC = 10;
+        private const double DIR = 1;
+        private const double XLOCAFTERMOVE = 501;
+        private const double YLOCAFTERMOVE = -199;
+        
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Box box = new BoxImpl();
+            BoxImpl box2 = new BoxImpl("grass.png", XLOC, YLOC);
+
+            box2.XDir = DIR;
+            box2.YDir = DIR;
+            box2.Move();
+
+            Assert.AreEqual(null, box.GetImage());
+            Assert.AreEqual(0, box.XLoc);
+            Assert.AreEqual(0, box.YLoc);
+
+            
+            Assert.AreEqual(XLOCAFTERMOVE, box2.XLoc);
+            Assert.AreEqual(YLOCAFTERMOVE, box2.YLoc);
+        }
+    }
+}
