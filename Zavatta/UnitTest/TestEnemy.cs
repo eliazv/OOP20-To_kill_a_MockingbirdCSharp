b3400f90fe6291eda1ed8df3dec0bf0260b98d22
Elia Zavatta <73821447+eliazv@users.noreply.github.com>
2021-07-07 11:27:36 +0200

    Add files via upload

---
+++ Zavatta/UnitTest/TestEnemy.cs
@@ -0,0 +1,45 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zavatta;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class TestEnemy
+    {
+        
+        private const int VEHICLE_SCROLL = 1;
+        private const int CAR_SPEED = 2;
+        private const int TRUCK_SPEED = 1;
+        private const int TRAIN_SPEED = 10;
+        private const int RANDOM_SPEED_ADDITION = 10;
+        private const int RANDOM_XLOCSPAWN_ADDITION = 1500;
+
+        [TestMethod]
+        public void TestCar()
+        {
+            IVehicle vehicle = new Vehicle();
+            vehicle.InitializeCar(4);
+            //Assert.AreEqual(4, vehicle.GetVehicle.YLoc);
+            //Assert.AreEqual(VEHICLE_SCROLL, vehicle.GetVehicle.YDir);
+            Assert.IsTrue(vehicle.GetVehicleType == VehicleType.TRUCK || vehicle.GetVehicleType == VehicleType.CAR);
+            Assert.IsTrue(vehicle.GetVehicleSpeed >= TRUCK_SPEED
+                    && vehicle.GetVehicleSpeed <= CAR_SPEED + RANDOM_SPEED_ADDITION);
+            Assert.IsTrue(vehicle.VehicleXLocSpawn >= TRUCK_SPEED
+                    && vehicle.VehicleXLocSpawn <= CAR_SPEED * RANDOM_XLOCSPAWN_ADDITION);
+        }
+
+        [TestMethod]
+        public void TestTrain()
+        {
+            IVehicle vehicle = new Vehicle();
+            vehicle.InitializeTrain(4);
+            //Assert.AreEqual(4, vehicle.GetVehicle.YLoc());
+            //Assert.AreEqual(VEHICLE_SCROLL, vehicle.GetVehicle.YDir);
+            Assert.AreEqual(VehicleType.TRAIN, vehicle.GetVehicleType);
+            Assert.IsTrue(vehicle.GetVehicleSpeed >= TRAIN_SPEED
+                    && vehicle.GetVehicleSpeed <= TRAIN_SPEED + RANDOM_SPEED_ADDITION);
+            Assert.IsTrue(vehicle.VehicleXLocSpawn >= TRAIN_SPEED
+                    && vehicle.VehicleXLocSpawn <= TRAIN_SPEED * RANDOM_XLOCSPAWN_ADDITION);
+        }
+    }
+}
---
+++ Zavatta/UnitTest/TestStrip.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zavatta;
+
+namespace UnitTest
+{
+    [TestClass]
+    class TestStrip
+    {
+        [TestMethod]
+        public void testStrip()
+        {
+            IStrip strip = new Strip();
+
+            strip.InitializeRndStrip(6);
+            Assert.AreEqual(8, strip.GetStrip.Count);
+
+            strip.InitializeSpecificStrip("Grass.png", "Tree.png", 2);
+            Assert.AreEqual(8, strip.GetStrip.Count);
+            Assert.AreEqual(StripEnvironment.GRASS, strip.GetStripEnvironment);
+
+            strip.InitializeSpecificStrip("Rail.png", 5);
+            Assert.AreEqual(8, strip.GetStrip.Count);
+            Assert.AreEqual("Rail.png", strip.GetBoxOfStrip(2).GetImage().getFileName());
+            Assert.AreEqual(StripEnvironment.RAIL, strip.GetStripEnvironment);
+
+        }
+    }
+}
