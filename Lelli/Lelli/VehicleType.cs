c46670ab9c8fea130d57cf780c7944b52d9b7381
PietroLelli <73821770+PietroLelli@users.noreply.github.com>
2021-07-08 16:20:13 +0200

    Add files via upload

---
+++ Lelli/Lelli/VehicleType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lelli
+{
+    public enum VehicleType
+    {
+        /**
+         * Identify a car.
+         */
+        CAR,
+
+        /**
+         * Identify a camion.
+         */
+        TRUCK,
+
+        /**
+         * Identify a train.
+         */
+        TRAIN
+    }
+}
